1337e629b9e1311256bfc2c3e7ad70c4012ba170
Brody Coan <bcoan2@una.edu>
2024-01-24 22:04:50 -0600

    yeet

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const app = express()
 
 app.get('/', function (req, res) {
   
-    res.send('Hello World from Express this is a test ')
+    res.send('Hello World from Express ')
+})
+
+app.get('Tha ennegram', function (req, res) {
+  
+  res.send('I am a number 8. The Master, The Challenger, The Defender, whatever you wanna call it. The definition is: Type 8s are natural leaders, thriving in situations where they can take charge. They champion fairness and justice and aren\'t afraid to push boundaries when they believe it\'s necessary. ')
+})
+
+app.get('Tha teams', function (req, res) {
+  
+  res.send(' 1 good thing about teams is you have a group of people you can bounce ideas off of and help the workload. one bad thing is the coordination or performance that some may have. only somtimes tho. ')
 })
 
 app.listen(3000)
